a30f1f27d7dc5cdacb32338546e107742a08dc35
Osmarcheeto <osmar.chavez@icloud.com>
2026-01-26 13:38:47 -0600

    Update 1_.py

--- 1_.py
+++ 1_.py
@@ -2,8 +2,72 @@
 # Remove the characters to the left of our main text:
 
 # ,
+#function = A block reusable code
+# place () after the function name to invoke it
+# print("Happy birhtday to you!")
+# print("You are old")
+# print("Happy birthday to you!")
 
+
+# print("Happy birhtday to you!")
+# print("You are old")
+# print("Happy birthday to you!")
+
+
+# print("Happy birhtday to you!")
+# print("You are old")
+# print("Happy birthday to you!")
+
+
+# def happy_birthday(name, uge):
+#     print(f"Happy  Birhtday to {name}!")
+# print(f"You are {uge} years old")
+# print("Happy birthday to you!")
+
+
+# happy_birthday("bro, 20")
+# happy_birthday("Stave, 35")
+# happy_birthday("Joe, 25")
+
+# def display_invoice(username, amount, due_date)
+#     print(f"Hello {username}")
+#     print(f"Your bill of ${amount:.2f} is due: {due_date}")
+
+# display_invoice("JoeSchmo", 100.01, "01/02")
 # :
+# def add(x, y):
+#     z = x + y
+#     return z
+
+# def subtract(x,y):
+#     z = x - y
+#     return z
+
+# def multiply(x, y):
+#     z = x * y
+#     return z
+
+# def divide(x, y):
+#     z = x / y
+#     return z
+
+# print(add(1, 2))
+# print(subtract(1, 2))
+# print(multiply(1, 2))
+# print(divide(1,2))
+
+def create_name(first, last):
+    first = first.capitalize()
+    last = last.capitalize()
+    return first + " " + last
+
+full_name = create_name ("bro", "code")
+
+print(full_name)
+
+
+
+
 
 # %
 
