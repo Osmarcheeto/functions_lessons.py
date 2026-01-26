69c391fa120ce6f32a8426562fca71a5f42cca6a
Osmarcheeto <osmar.chavez@icloud.com>
2026-01-26 13:31:54 -0600

    Update 2_functions.py

--- 2_functions.py
+++ 2_functions.py
@@ -3,6 +3,10 @@
 
 # You should only define the function, you should not call it later.
 
+def greet():
+    print("Hello world!")
+
+
 
 
 
@@ -15,7 +19,10 @@
 # Create a Variable: Define a variable named name and store any name of your choice in it.
 
 # Note: Only define the function and create the variable. Do not call the function afterwards.
+def welcome(name):
+    print(f"Welcome {name}!")
 
+name = "Osmar"
 
 
 
@@ -27,3 +34,7 @@
 # The name of the argument that this function must take is number. Create this variable and assign it any number.
 
 # You should only define the function and create the variable, you should not call the function afterwards.
+def square(number):
+    print(number ** 2)
+
+number = 4
