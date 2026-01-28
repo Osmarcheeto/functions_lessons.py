d0148e4f4ea076c7cca63bc7e278fc8a6324d4f3
Osmarcheeto <osmar.chavez@icloud.com>
2026-01-28 13:28:20 -0600

    Add functions to count and list keyword arguments

--- 8_indefinite_arguments_kwargs.py
+++ 8_indefinite_arguments_kwargs.py
@@ -1,7 +1,15 @@
 
 # Indefinite Arguments (**kwargs) Practice #1
 # Create a function called number_attributes that counts the number of parameters that are passed, and returns that number as the result.
-
+def number_attributes(**kwargs):
+    count = 0
+    for key in kwargs:
+        count += 1
+        # first time through: count = 0 + 1 = 1
+        # second time through: count = 1 + 1 = 2
+        # third time through: count = 2 + 1 = 3
+    return count    
+print(number_attributes(height=180, weight=75, age=30)) # Should return 3   
 
 
 
@@ -12,6 +20,13 @@
 
 # Indefinite Arguments (**kwargs) Practice #2
 # Create a function called list_attributes that returns in the form of a list the values of the attributes given in the form of keywords. The function must expect to receive any number of arguments of this type.
+def list_attributes(**kwargs):
+    values_list = []
+    for value in kwargs.values():
+        values_list.append(value)
+    return values_list
+print(list_attributes(hobby="painting", city="New York", profession="engineer"))
+# Should return ["painting", "New York", "engineer"]
 
 
 
